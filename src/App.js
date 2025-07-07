5101119b31a6fa7dc506e24ba0f72fd693c72950
Rahil <rahilpatial161381@gmail.com>
2025-07-07 18:21:08 +0530

    Fix: moved image reference to App.js inline style using PUBLIC_URL

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/pexels-pixelcop-2799556.jpg)`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+    >
       <HomePage />
     </div>
   );
 }
 
 export default App;
-
